f2352c7758d4bfb2329f5a3cbba67ea9b7573548
Gabriel Frantz <gabriel.frantz@universo.univates.br>
2022-04-11 01:11:03 -0300

    add humanplayer and methods

--- tic_tac_toe_ai/tic-tac-toe.py
+++ tic_tac_toe_ai/tic-tac-toe.py
@@ -58,3 +58,83 @@ class Board:
 
 def start() -> None:
   board = Board(3)
+
+class HumanPlayer:
+    def __init__(self, stepSize = 0.1, exploreRate=0.1):
+        self.symbol = None
+        self.currentState = None
+        return
+    def reset(self):
+        return
+    def setSymbol(self, symbol):
+        self.symbol = symbol
+        return
+    def feedState(self, state):
+        self.currentState = state
+        return
+    def feedReward(self, reward):
+        return
+    def takeAction(self):
+        data = int(input("Input your position:"))
+        data -= 1
+        i = data
+        j = data
+        if self.currentState.data[i, j] != 0:
+            return self.takeAction()
+        return (i, j, self.symbol)
+
+def train(epochs=20000):
+    player1 = Player()
+    player2 = Player()
+    judger = Judger(player1, player2)
+    player1Win = 0.0
+    player2Win = 0.0
+    for i in range(0, epochs):
+        print("Epoch", i)
+        winner = judger.play()
+        if winner == 1:
+            player1Win += 1
+        if winner == -1:
+            player2Win += 1
+        judger.reset()
+    print(player1Win / epochs)
+    print(player2Win / epochs)
+    player1.savePolicy()
+    player2.savePolicy()
+
+def compete(turns=500):
+    player1 = Player(exploreRate=0)
+    player2 = Player(exploreRate=0)
+    judger = Judger(player1, player2, False)
+    player1.loadPolicy()
+    player2.loadPolicy()
+    player1Win = 0.0
+    player2Win = 0.0
+    for i in range(0, turns):
+        print("Epoch", i)
+        winner = judger.play()
+        if winner == 1:
+            player1Win += 1
+        if winner == -1:
+            player2Win += 1
+        judger.reset()
+    print(player1Win / turns)
+    print(player2Win / turns)
+
+def play():
+    while True:
+        player1 = Player(exploreRate=0)
+        player2 = HumanPlayer()
+        judger = Judger(player1, player2, False)
+        player1.loadPolicy()
+        winner = judger.play(True)
+        if winner == player2.symbol:
+            print("Win!")
+        elif winner == player1.symbol:
+            print("Lose!")
+        else:
+            print("Tie!")
+
+train()
+compete()
+play() 
